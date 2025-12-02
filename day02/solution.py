79e5f00177f3b734e6206f386fefcc40d860db47
justanotherbyte <reachvishm@gmail.com>
2025-12-02 12:24:19 +0000

    smallify day 2

--- day02/solution.py
+++ day02/solution.py
@@ -1,44 +1,32 @@
 import re
 
 def is_invalid(n: int):
-    ms = (re.findall(r"\b(\d+)\1+\b", str(n)))
+    ms = re.findall(r"\b(\d+)\1+\b", str(n))
     if ms:
         m = ms[0]
-        return str(n).count(m) == 2
+        return str(n).count(m)
     
-    return False
-
-def is_invalid2(n: int):
-    ms = (re.findall(r"\b(\d+)\1+\b", str(n)))
-    if ms:
-        m = ms[0]
-        return str(n).count(m) >= 2
-    
-    return False
+    return 0
 
 
 def part1(f: str):
-    rs = f.split(",")
     s = 0
 
-    for r in rs:
+    for r in f.split(","):
         a, b = map(int, r.split("-"))
 
         for n in range(a, b+1):
-            if is_invalid(n):
-                s += n
+            s += n if is_invalid(n) == 2 else 0
 
     print(s)
 
 def part2(f: str):
-    rs = f.split(",")
     s = 0
 
-    for r in rs:
+    for r in f.split(","):
         a, b = map(int, r.split("-"))
 
         for n in range(a, b+1):
-            if is_invalid2(n):
-                s += n
+            s += n if is_invalid(n) >= 2 else 0
 
     print(s)
